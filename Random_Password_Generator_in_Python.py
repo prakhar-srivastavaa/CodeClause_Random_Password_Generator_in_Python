62ff81b66c529be56cee6fcb45879c70f18e39f0
Prakhar Srivastava <73735817+prakhar-srivastavaa@users.noreply.github.com>
2023-05-05 15:32:50 +0530

    Update Random_Password_Generator_in_Python.py

--- Random_Password_Generator_in_Python.py
+++ Random_Password_Generator_in_Python.py
@@ -1,57 +1,110 @@
 ## Random Password Generator in Python 
 
 
-import string
+# Python program to generate random
+# Random password Generator using Tkinter module
 import random
+import pyperclip
+from tkinter import *
+from tkinter.ttk import *
 
-# Typing the password length you want
+# Function for calculation of password
 
-length = int(input("Enter The password length You Wish For: "))
 
-print('''Choose the character set for password from these :
-		1. Digits
-		2. Letters
-		3. Special characters	
-		4. Exit''')
+def low():
+	entry.delete(0, END)
 
-character_List = ""
+	# Get the length of password
+	length = var1.get()
 
-# Getting the character set for password
+	lower = "abcdefghijklmnopqrstuvwxyz"
+	upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	digits = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 !@#$%^&*()"
+	password = ""
 
-### Conditional loop
+	# if strength selected is low
+	if var.get() == 1:
+		for i in range(0, length):
+			password = password + random.choice(lower)
+		return password
 
-while(True):
-	choice = int(input("Pick a number "))
-	if(choice == 1):
-		
-		# Adding the letters to possible characters
-		character_List += string.ascii_letters
-	elif(choice == 2):
-		
-		# Adding the digits to possible characters
-		character_List += string.digits
-	elif(choice == 3):
-		
-		# Adding the special characters to possible
-		# characters
-		character_List += string.punctuation
-	elif(choice == 4):
-		break
+	# if strength selected is medium
+	elif var.get() == 0:
+		for i in range(0, length):
+			password = password + random.choice(upper)
+		return password
+
+	# if strength selected is strong
+	elif var.get() == 3:
+		for i in range(0, length):
+			password = password + random.choice(digits)
+		return password
 	else:
-		print("Please pick a valid option!")
+		print("Please choose an option")
 
-password = []
 
-for i in range(length):
+# Function for generation of password
+def generate():
+	password1 = low()
+	entry.insert(10, password1)
 
-	# Picking a random character from our
-	# character list
-	randomchar = random.choice(character_List)
-	
-	# appending a random character to password
-	password.append(randomchar)
 
-# printing password as a string
-print("The random password is " + "".join(password))
+# Function for copying password to clipboard
+def copy1():
+	random_password = entry.get()
+	pyperclip.copy(random_password)
 
-# Thankyou # 
+
+# Main Function
+
+# create GUI window
+root = Tk()
+var = IntVar()
+var1 = IntVar()
+
+# Title of your GUI window
+root.title("Random Password Generator")
+
+# create label and entry to show
+# password generated
+Random_password = Label(root, text="Password")
+Random_password.grid(row=0)
+entry = Entry(root)
+entry.grid(row=0, column=1)
+
+# create label for length of password
+c_label = Label(root, text="Length")
+c_label.grid(row=1)
+
+# create Buttons Copy which will copy
+# password to clipboard and Generate
+# which will generate the password
+copy_button = Button(root, text="Copy", command=copy1)
+copy_button.grid(row=0, column=2)
+generate_button = Button(root, text="Generate", command=generate)
+generate_button.grid(row=0, column=3)
+
+# Radio Buttons for deciding the
+# strength of password
+# Default strength is Medium
+radio_low = Radiobutton(root, text="Low", variable=var, value=1)
+radio_low.grid(row=1, column=2, sticky='E')
+radio_middle = Radiobutton(root, text="Medium", variable=var, value=0)
+radio_middle.grid(row=1, column=3, sticky='E')
+radio_strong = Radiobutton(root, text="Strong", variable=var, value=3)
+radio_strong.grid(row=1, column=4, sticky='E')
+combo = Combobox(root, textvariable=var1)
+
+# Combo Box for length of your password
+combo['values'] = (8, 9, 10, 11, 12, 13, 14, 15, 16,
+				17, 18, 19, 20, 21, 22, 23, 24, 25,
+				26, 27, 28, 29, 30, 31, 32, "Length")
+combo.current(0)
+combo.bind('<<ComboboxSelected>>')
+combo.grid(column=1, row=1)
+
+# start the GUI
+root.mainloop()
+
+
+# Thankyou #
